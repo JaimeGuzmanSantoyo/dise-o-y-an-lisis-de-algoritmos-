4f4c2b4724c1c86e0191dc6b02d0cceccd07a94a
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2023-08-31 19:28:12 -0600

    tarea 5 Jose Jaime Guzman Santoyo

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,13 +1,18 @@
 function ValorMinimo(arreglo){
 let minimo = arreglo[0]
-for( let i =0;i<arreglo.length;i++){
+let maximoValor = arreglo[0]
+for( let i =0;i<arreglo.length;i++){ //para ir incrementando la variable i hasta el tamaño del array
 if (minimo > arreglo[i]){
 minimo =  arreglo[i]
 
 }
+if ( maximoValor< arreglo[i]){
+    maximoValor =  arreglo[i]
 }
-return [minimo]
+}
+return [minimo,maximoValor] // imorimir valores minimos y maximos
+
+
 }
 ValorMinimo([20,0,5,30,2])
-
 
