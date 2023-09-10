d4c65436c1aebb1d699c0e140c152e5ffcd31b32
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2023-09-09 22:09:54 -0600

    tarea promedio

--- Tarea6.js
+++ Tarea6.js
@@ -1,11 +1,23 @@
 var arreglo = [3,5,9,13];
 let proedio = 0;
+let resultado = 0;
 
-let res = arreglo.reduce(
+
+for ( let i = 0 ;i < array.length ;i++) {
+    if (resultado< arreglo[i]){
+        resultado =  arreglo[i]
+        resultado = arreglo.reduce(
     (sum,item) => sum +item ,
     0
 );
-console.log(res);
+        }
+    
+}
+ /*res = arreglo.reduce(
+    (sum,item) => sum +item ,
+    0
+);*/
+console.log(resultado);
 
-proedio = res / arreglo.length 
+proedio = resultado / arreglo.length 
 console.log(proedio)
